cbb8cbc6cf2e0e574f8efe26345e666e04c06681
Mark Wu <88671288+markw551@users.noreply.github.com>
2023-02-21 13:02:19 -0500

    Basic character movement
    Character can jump, move left and right

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float dirX = Input.GetAxis("Horizontal");
+        rb.velocity = new UnityEngine.Vector2(dirX * 7f, rb.velocity.y); //velocity y is kept instead of resetting to zero and causing character to jump
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new UnityEngine.Vector2(rb.velocity.x, 14f);
+        }
+    }
+}
+

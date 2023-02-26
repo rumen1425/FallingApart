aac876c163ec9c5e2e1592ddbf392a6b31f2ad0f
Mark Wu <88671288+markw551@users.noreply.github.com>
2023-02-26 16:52:23 -0500

    working coin counter, in progress health bar

--- Assets/Scripts/itemcollector.cs
+++ Assets/Scripts/itemcollector.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class itemcollector : MonoBehaviour
 {
     private int coins = 0;
 
+    [SerializeField] private Text coinsText; 
+     
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("coins"))
         {
             Destroy(collision.gameObject);
             coins++;
-            Debug.Log("Coins: " + coins);
+            coinsText.text = "Coins: " + coins;
         }
     }
 }
--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Numerics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class movement : MonoBehaviour
 {
+    int health = 3;
+    bool jump = false;
+
     private Rigidbody2D rb;
 
     // Start is called before the first frame update
@@ -16,16 +21,25 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        float dirX = Input.GetAxis("Horizontal");
-        rb.velocity = new UnityEngine.Vector2(dirX * 15f, rb.velocity.y); //velocity y is kept instead of resetting to zero and causing character to jump
+        float directionX = Input.GetAxis("Horizontal");
+        rb.velocity = new UnityEngine.Vector2(directionX * 15f, rb.velocity.y); //velocity y is kept instead of resetting to zero and causing character to jump
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && (jump == false))
         {
             rb.velocity = new UnityEngine.Vector2(rb.velocity.x, 20f);
+            jump = true;
+        }
+
+        if (rb.velocity.y == 0)
+        {
+            jump = false;
         }
     }
-
-
-
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("mobs"))
+    //    {
+    //        health -= 1; 
+    //    }
+    //}
 }
-

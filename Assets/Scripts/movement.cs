e8628a472bb5f785e1578a4fd94ab39bffe1b25d
Christopher Ying <71330973+ChristopherYing@users.noreply.github.com>
2023-02-25 20:13:17 -0500

    Add parallax scrolling to backgrounds

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BackgroundScroll[] backgrounds;
 
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        backgrounds = FindObjectsOfType<BackgroundScroll>();
     }
 
     // Update is called once per frame
@@ -18,6 +20,12 @@ public class movement : MonoBehaviour
     {
         float dirX = Input.GetAxis("Horizontal");
         rb.velocity = new UnityEngine.Vector2(dirX * 15f, rb.velocity.y); //velocity y is kept instead of resetting to zero and causing character to jump
+
+        // Scroll every background.
+        foreach(var bg in backgrounds)
+        {
+            bg.scroll(dirX);
+        }
 
         if (Input.GetButtonDown("Jump"))
         {

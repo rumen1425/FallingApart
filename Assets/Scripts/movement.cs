273bbf59c7b03d21c892a97b867e0bc04197b6ec
Mark Wu <88671288+markw551@users.noreply.github.com>
2023-02-26 18:08:05 -0500

    redid health bar and coin collection after merge issues

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Numerics;
 using UnityEngine;
 
@@ -7,6 +8,8 @@ public class movement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private BackgroundScroll[] backgrounds;
+    private bool jump = true;
+    public int health = 5;
 
     // Start is called before the first frame update
     private void Start()
@@ -22,18 +25,28 @@ public class movement : MonoBehaviour
         rb.velocity = new UnityEngine.Vector2(dirX * 15f, rb.velocity.y); //velocity y is kept instead of resetting to zero and causing character to jump
 
         // Scroll every background.
-        foreach(var bg in backgrounds)
+        foreach (var bg in backgrounds)
         {
             bg.scroll(dirX);
         }
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && (jump == true))
         {
             rb.velocity = new UnityEngine.Vector2(rb.velocity.x, 20f);
+            jump = false;
+        }
+
+        if (rb.velocity.y == 0)
+        {
+            jump = true;
         }
     }
 
-
-
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("mobs"))
+        {
+            health -= 1;
+        }
+    }
 }
-
